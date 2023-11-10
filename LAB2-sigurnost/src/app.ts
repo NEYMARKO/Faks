442a37651b8d62c3928903642ce2dd95adcff188
NEYMARKO <marko.prosenjak5@gmail.com>
2023-11-10 15:52:05 +0100

    Linkan css s programom

--- LAB2-sigurnost/src/app.ts
+++ LAB2-sigurnost/src/app.ts
@@ -2,9 +2,9 @@ import express from 'express';
 import path from 'path';
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set("views", path.join(__dirname, "views"));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {    
     res.render('index');
