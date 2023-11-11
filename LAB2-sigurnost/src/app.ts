d4bc92b9e4548025d7e2d5b8af1194db24ee0f63
NEYMARKO <marko.prosenjak5@gmail.com>
2023-11-11 13:06:21 +0100

    Dodana opcija umetanja XSS-a

--- LAB2-sigurnost/src/app.ts
+++ LAB2-sigurnost/src/app.ts
@@ -1,14 +1,29 @@
 import express from 'express';
 import path from 'path';
+import bodyParser from 'body-parser'; // Import body-parser
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {    
-    res.render('index');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.render('index');
 });
+
+app.route('/submit')
+  .get((req, res) => {
+    const name = req.query.name || '';
+    res.type('text/html').send(`<h1>Hello, ${name}!</h1>`);
+  })
+  .post((req, res) => {
+    const name = req.body.name || '';
+    res.send(`<h1>Hello, ${name}!</h1>`);
+  });
 
 const hostname = '127.0.0.1';
 const port = 4072;
--- LAB2-sigurnost/src/public/js/site.js
+++ LAB2-sigurnost/src/public/js/site.js
@@ -2,8 +2,7 @@ var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.6.3.min.js';
 document.getElementsByTagName('head')[0].appendChild(script);
 var warningText = "Ovo je tekst koji objašnjava kako ovaj napad funkcionira. Najvjerojatnije ću imati nekakvu formu preko koje ću izvršiti napad";
-
-
+document.cookie = "COOKIE";
 function handleDefense() {
     //console.log("IN HANDLE DEFENSE");
     var checkBox = $('.form-check-input');
